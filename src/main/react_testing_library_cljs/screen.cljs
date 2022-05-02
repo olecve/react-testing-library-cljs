4178fd3060d14519c947dd5dc84582628dab3e71
Oleg Tsvetkov <olecve@gmail.com>
2022-05-03 01:18:21 +0300

    Provide generated source file with all queries

--- src/main/react_testing_library_cljs/screen.cljs
+++ src/main/react_testing_library_cljs/screen.cljs
@@ -3,4 +3,147 @@
     ["@testing-library/react" :refer [screen]]))
 
 ;; Begin - Generated Code (Do not modify manually)
+(defn get-by-role [query]
+  (.getByRole screen query))
+
+(defn get-by-label-text [query]
+  (.getByLabelText screen query))
+
+(defn get-by-placeholder-text [query]
+  (.getByPlaceholderText screen query))
+
+(defn get-by-text [query]
+  (.getByText screen query))
+
+(defn get-by-display-value [query]
+  (.getByDisplayValue screen query))
+
+(defn get-by-alt-text [query]
+  (.getByAltText screen query))
+
+(defn get-by-title [query]
+  (.getByTitle screen query))
+
+(defn get-by-test-id [query]
+  (.getByTestId screen query))
+
+(defn query-by-role [query]
+  (.queryByRole screen query))
+
+(defn query-by-label-text [query]
+  (.queryByLabelText screen query))
+
+(defn query-by-placeholder-text [query]
+  (.queryByPlaceholderText screen query))
+
+(defn query-by-text [query]
+  (.queryByText screen query))
+
+(defn query-by-display-value [query]
+  (.queryByDisplayValue screen query))
+
+(defn query-by-alt-text [query]
+  (.queryByAltText screen query))
+
+(defn query-by-title [query]
+  (.queryByTitle screen query))
+
+(defn query-by-test-id [query]
+  (.queryByTestId screen query))
+
+(defn find-by-role [query]
+  (.findByRole screen query))
+
+(defn find-by-label-text [query]
+  (.findByLabelText screen query))
+
+(defn find-by-placeholder-text [query]
+  (.findByPlaceholderText screen query))
+
+(defn find-by-text [query]
+  (.findByText screen query))
+
+(defn find-by-display-value [query]
+  (.findByDisplayValue screen query))
+
+(defn find-by-alt-text [query]
+  (.findByAltText screen query))
+
+(defn find-by-title [query]
+  (.findByTitle screen query))
+
+(defn find-by-test-id [query]
+  (.findByTestId screen query))
+
+(defn get-all-by-role [query]
+  (.getAllByRole screen query))
+
+(defn get-all-by-label-text [query]
+  (.getAllByLabelText screen query))
+
+(defn get-all-by-placeholder-text [query]
+  (.getAllByPlaceholderText screen query))
+
+(defn get-all-by-text [query]
+  (.getAllByText screen query))
+
+(defn get-all-by-display-value [query]
+  (.getAllByDisplayValue screen query))
+
+(defn get-all-by-alt-text [query]
+  (.getAllByAltText screen query))
+
+(defn get-all-by-title [query]
+  (.getAllByTitle screen query))
+
+(defn get-all-by-test-id [query]
+  (.getAllByTestId screen query))
+
+(defn query-all-by-role [query]
+  (.queryAllByRole screen query))
+
+(defn query-all-by-label-text [query]
+  (.queryAllByLabelText screen query))
+
+(defn query-all-by-placeholder-text [query]
+  (.queryAllByPlaceholderText screen query))
+
+(defn query-all-by-text [query]
+  (.queryAllByText screen query))
+
+(defn query-all-by-display-value [query]
+  (.queryAllByDisplayValue screen query))
+
+(defn query-all-by-alt-text [query]
+  (.queryAllByAltText screen query))
+
+(defn query-all-by-title [query]
+  (.queryAllByTitle screen query))
+
+(defn query-all-by-test-id [query]
+  (.queryAllByTestId screen query))
+
+(defn find-all-by-role [query]
+  (.findAllByRole screen query))
+
+(defn find-all-by-label-text [query]
+  (.findAllByLabelText screen query))
+
+(defn find-all-by-placeholder-text [query]
+  (.findAllByPlaceholderText screen query))
+
+(defn find-all-by-text [query]
+  (.findAllByText screen query))
+
+(defn find-all-by-display-value [query]
+  (.findAllByDisplayValue screen query))
+
+(defn find-all-by-alt-text [query]
+  (.findAllByAltText screen query))
+
+(defn find-all-by-title [query]
+  (.findAllByTitle screen query))
+
+(defn find-all-by-test-id [query]
+  (.findAllByTestId screen query))
 ;; End - Generated Code (Do not modify manually)
