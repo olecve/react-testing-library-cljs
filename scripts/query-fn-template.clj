f51abc22ac069a09035635e8225e387ffdb50a81
Oleg Tsvetkov <olecve@gmail.com>
2022-05-11 20:24:42 +0300

    Update query template

--- scripts/query-fn-template.clj
+++ scripts/query-fn-template.clj
@@ -1,2 +1,5 @@
-(defn $cljs-fn-name [query]
-  (.$js-fn-name screen query))
+(defn $cljs-fn-name
+  ([query]
+   (.$js-fn-name screen query))
+  ([query options]
+   (.$js-fn-name screen query (clj->js options))))
