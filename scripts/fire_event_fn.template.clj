adf717d87cb6f7d2af79ce3d3ba5a718efadafa3
Oleg Tsvetkov <olecve@gmail.com>
2023-07-11 22:46:23 +0300

    Make option arg optional

--- scripts/fire_event_fn.template.clj
+++ scripts/fire_event_fn.template.clj
@@ -1,2 +1,5 @@
-(defn $cljs-fn-name [element options]
-  (.-$js-event-type fireEvent element options))
+(defn $cljs-fn-name
+  ([element]
+   (.-$js-event-type fireEvent element))
+  ([element options]
+   (.-$js-event-type fireEvent element options)))
--- src/main/react_testing_library_cljs/fire_event.cljs
+++ src/main/react_testing_library_cljs/fire_event.cljs
@@ -4,252 +4,501 @@
 
 ;; Refer to https://testing-library.com/docs/dom-testing-library/api-events/ for more details about events
 ;; Begin - Generated Code (Do not modify manually)
-(defn copy [element options]
-  (.-copy fireEvent element options))
-
-(defn cut [element options]
-  (.-cut fireEvent element options))
-
-(defn paste [element options]
-  (.-paste fireEvent element options))
-
-(defn compositionEnd [element options]
-  (.-compositionEnd fireEvent element options))
-
-(defn compositionStart [element options]
-  (.-compositionStart fireEvent element options))
-
-(defn compositionUpdate [element options]
-  (.-compositionUpdate fireEvent element options))
-
-(defn keyDown [element options]
-  (.-keyDown fireEvent element options))
-
-(defn keyPress [element options]
-  (.-keyPress fireEvent element options))
-
-(defn keyUp [element options]
-  (.-keyUp fireEvent element options))
-
-(defn focus [element options]
-  (.-focus fireEvent element options))
-
-(defn blur [element options]
-  (.-blur fireEvent element options))
-
-(defn focusIn [element options]
-  (.-focusIn fireEvent element options))
-
-(defn focusOut [element options]
-  (.-focusOut fireEvent element options))
-
-(defn change [element options]
-  (.-change fireEvent element options))
-
-(defn input [element options]
-  (.-input fireEvent element options))
-
-(defn invalid [element options]
-  (.-invalid fireEvent element options))
-
-(defn submit [element options]
-  (.-submit fireEvent element options))
-
-(defn reset [element options]
-  (.-reset fireEvent element options))
-
-(defn click [element options]
-  (.-click fireEvent element options))
-
-(defn contextMenu [element options]
-  (.-contextMenu fireEvent element options))
-
-(defn dblClick [element options]
-  (.-dblClick fireEvent element options))
-
-(defn drag [element options]
-  (.-drag fireEvent element options))
-
-(defn dragEnd [element options]
-  (.-dragEnd fireEvent element options))
-
-(defn dragEnter [element options]
-  (.-dragEnter fireEvent element options))
-
-(defn dragExit [element options]
-  (.-dragExit fireEvent element options))
-
-(defn dragLeave [element options]
-  (.-dragLeave fireEvent element options))
-
-(defn dragOver [element options]
-  (.-dragOver fireEvent element options))
-
-(defn dragStart [element options]
-  (.-dragStart fireEvent element options))
-
-(defn drop [element options]
-  (.-drop fireEvent element options))
-
-(defn mouseDown [element options]
-  (.-mouseDown fireEvent element options))
-
-(defn mouseEnter [element options]
-  (.-mouseEnter fireEvent element options))
-
-(defn mouseLeave [element options]
-  (.-mouseLeave fireEvent element options))
-
-(defn mouseMove [element options]
-  (.-mouseMove fireEvent element options))
-
-(defn mouseOut [element options]
-  (.-mouseOut fireEvent element options))
-
-(defn mouseOver [element options]
-  (.-mouseOver fireEvent element options))
-
-(defn mouseUp [element options]
-  (.-mouseUp fireEvent element options))
-
-(defn popState [element options]
-  (.-popState fireEvent element options))
-
-(defn select [element options]
-  (.-select fireEvent element options))
-
-(defn touchCancel [element options]
-  (.-touchCancel fireEvent element options))
-
-(defn touchEnd [element options]
-  (.-touchEnd fireEvent element options))
-
-(defn touchMove [element options]
-  (.-touchMove fireEvent element options))
-
-(defn touchStart [element options]
-  (.-touchStart fireEvent element options))
-
-(defn scroll [element options]
-  (.-scroll fireEvent element options))
-
-(defn wheel [element options]
-  (.-wheel fireEvent element options))
-
-(defn abort [element options]
-  (.-abort fireEvent element options))
-
-(defn canPlay [element options]
-  (.-canPlay fireEvent element options))
-
-(defn canPlayThrough [element options]
-  (.-canPlayThrough fireEvent element options))
-
-(defn durationChange [element options]
-  (.-durationChange fireEvent element options))
-
-(defn emptied [element options]
-  (.-emptied fireEvent element options))
-
-(defn encrypted [element options]
-  (.-encrypted fireEvent element options))
-
-(defn ended [element options]
-  (.-ended fireEvent element options))
-
-(defn loadedData [element options]
-  (.-loadedData fireEvent element options))
-
-(defn loadedMetadata [element options]
-  (.-loadedMetadata fireEvent element options))
-
-(defn loadStart [element options]
-  (.-loadStart fireEvent element options))
-
-(defn pause [element options]
-  (.-pause fireEvent element options))
-
-(defn play [element options]
-  (.-play fireEvent element options))
-
-(defn playing [element options]
-  (.-playing fireEvent element options))
-
-(defn progress [element options]
-  (.-progress fireEvent element options))
-
-(defn rateChange [element options]
-  (.-rateChange fireEvent element options))
-
-(defn seeked [element options]
-  (.-seeked fireEvent element options))
-
-(defn seeking [element options]
-  (.-seeking fireEvent element options))
-
-(defn stalled [element options]
-  (.-stalled fireEvent element options))
-
-(defn suspend [element options]
-  (.-suspend fireEvent element options))
-
-(defn timeUpdate [element options]
-  (.-timeUpdate fireEvent element options))
-
-(defn volumeChange [element options]
-  (.-volumeChange fireEvent element options))
-
-(defn waiting [element options]
-  (.-waiting fireEvent element options))
-
-(defn load [element options]
-  (.-load fireEvent element options))
-
-(defn error [element options]
-  (.-error fireEvent element options))
-
-(defn animationStart [element options]
-  (.-animationStart fireEvent element options))
-
-(defn animationEnd [element options]
-  (.-animationEnd fireEvent element options))
-
-(defn animationIteration [element options]
-  (.-animationIteration fireEvent element options))
-
-(defn transitionEnd [element options]
-  (.-transitionEnd fireEvent element options))
-
-(defn doubleClick [element options]
-  (.-doubleClick fireEvent element options))
-
-(defn pointerOver [element options]
-  (.-pointerOver fireEvent element options))
-
-(defn pointerEnter [element options]
-  (.-pointerEnter fireEvent element options))
-
-(defn pointerDown [element options]
-  (.-pointerDown fireEvent element options))
-
-(defn pointerMove [element options]
-  (.-pointerMove fireEvent element options))
-
-(defn pointerUp [element options]
-  (.-pointerUp fireEvent element options))
-
-(defn pointerCancel [element options]
-  (.-pointerCancel fireEvent element options))
-
-(defn pointerOut [element options]
-  (.-pointerOut fireEvent element options))
-
-(defn pointerLeave [element options]
-  (.-pointerLeave fireEvent element options))
-
-(defn gotPointerCapture [element options]
-  (.-gotPointerCapture fireEvent element options))
-
-(defn lostPointerCapture [element options]
-  (.-lostPointerCapture fireEvent element options))
+(defn copy
+  ([element]
+   (.-copy fireEvent element))
+  ([element options]
+   (.-copy fireEvent element options)))
+
+(defn cut
+  ([element]
+   (.-cut fireEvent element))
+  ([element options]
+   (.-cut fireEvent element options)))
+
+(defn paste
+  ([element]
+   (.-paste fireEvent element))
+  ([element options]
+   (.-paste fireEvent element options)))
+
+(defn compositionEnd
+  ([element]
+   (.-compositionEnd fireEvent element))
+  ([element options]
+   (.-compositionEnd fireEvent element options)))
+
+(defn compositionStart
+  ([element]
+   (.-compositionStart fireEvent element))
+  ([element options]
+   (.-compositionStart fireEvent element options)))
+
+(defn compositionUpdate
+  ([element]
+   (.-compositionUpdate fireEvent element))
+  ([element options]
+   (.-compositionUpdate fireEvent element options)))
+
+(defn keyDown
+  ([element]
+   (.-keyDown fireEvent element))
+  ([element options]
+   (.-keyDown fireEvent element options)))
+
+(defn keyPress
+  ([element]
+   (.-keyPress fireEvent element))
+  ([element options]
+   (.-keyPress fireEvent element options)))
+
+(defn keyUp
+  ([element]
+   (.-keyUp fireEvent element))
+  ([element options]
+   (.-keyUp fireEvent element options)))
+
+(defn focus
+  ([element]
+   (.-focus fireEvent element))
+  ([element options]
+   (.-focus fireEvent element options)))
+
+(defn blur
+  ([element]
+   (.-blur fireEvent element))
+  ([element options]
+   (.-blur fireEvent element options)))
+
+(defn focusIn
+  ([element]
+   (.-focusIn fireEvent element))
+  ([element options]
+   (.-focusIn fireEvent element options)))
+
+(defn focusOut
+  ([element]
+   (.-focusOut fireEvent element))
+  ([element options]
+   (.-focusOut fireEvent element options)))
+
+(defn change
+  ([element]
+   (.-change fireEvent element))
+  ([element options]
+   (.-change fireEvent element options)))
+
+(defn input
+  ([element]
+   (.-input fireEvent element))
+  ([element options]
+   (.-input fireEvent element options)))
+
+(defn invalid
+  ([element]
+   (.-invalid fireEvent element))
+  ([element options]
+   (.-invalid fireEvent element options)))
+
+(defn submit
+  ([element]
+   (.-submit fireEvent element))
+  ([element options]
+   (.-submit fireEvent element options)))
+
+(defn reset
+  ([element]
+   (.-reset fireEvent element))
+  ([element options]
+   (.-reset fireEvent element options)))
+
+(defn click
+  ([element]
+   (.-click fireEvent element))
+  ([element options]
+   (.-click fireEvent element options)))
+
+(defn contextMenu
+  ([element]
+   (.-contextMenu fireEvent element))
+  ([element options]
+   (.-contextMenu fireEvent element options)))
+
+(defn dblClick
+  ([element]
+   (.-dblClick fireEvent element))
+  ([element options]
+   (.-dblClick fireEvent element options)))
+
+(defn drag
+  ([element]
+   (.-drag fireEvent element))
+  ([element options]
+   (.-drag fireEvent element options)))
+
+(defn dragEnd
+  ([element]
+   (.-dragEnd fireEvent element))
+  ([element options]
+   (.-dragEnd fireEvent element options)))
+
+(defn dragEnter
+  ([element]
+   (.-dragEnter fireEvent element))
+  ([element options]
+   (.-dragEnter fireEvent element options)))
+
+(defn dragExit
+  ([element]
+   (.-dragExit fireEvent element))
+  ([element options]
+   (.-dragExit fireEvent element options)))
+
+(defn dragLeave
+  ([element]
+   (.-dragLeave fireEvent element))
+  ([element options]
+   (.-dragLeave fireEvent element options)))
+
+(defn dragOver
+  ([element]
+   (.-dragOver fireEvent element))
+  ([element options]
+   (.-dragOver fireEvent element options)))
+
+(defn dragStart
+  ([element]
+   (.-dragStart fireEvent element))
+  ([element options]
+   (.-dragStart fireEvent element options)))
+
+(defn drop
+  ([element]
+   (.-drop fireEvent element))
+  ([element options]
+   (.-drop fireEvent element options)))
+
+(defn mouseDown
+  ([element]
+   (.-mouseDown fireEvent element))
+  ([element options]
+   (.-mouseDown fireEvent element options)))
+
+(defn mouseEnter
+  ([element]
+   (.-mouseEnter fireEvent element))
+  ([element options]
+   (.-mouseEnter fireEvent element options)))
+
+(defn mouseLeave
+  ([element]
+   (.-mouseLeave fireEvent element))
+  ([element options]
+   (.-mouseLeave fireEvent element options)))
+
+(defn mouseMove
+  ([element]
+   (.-mouseMove fireEvent element))
+  ([element options]
+   (.-mouseMove fireEvent element options)))
+
+(defn mouseOut
+  ([element]
+   (.-mouseOut fireEvent element))
+  ([element options]
+   (.-mouseOut fireEvent element options)))
+
+(defn mouseOver
+  ([element]
+   (.-mouseOver fireEvent element))
+  ([element options]
+   (.-mouseOver fireEvent element options)))
+
+(defn mouseUp
+  ([element]
+   (.-mouseUp fireEvent element))
+  ([element options]
+   (.-mouseUp fireEvent element options)))
+
+(defn popState
+  ([element]
+   (.-popState fireEvent element))
+  ([element options]
+   (.-popState fireEvent element options)))
+
+(defn select
+  ([element]
+   (.-select fireEvent element))
+  ([element options]
+   (.-select fireEvent element options)))
+
+(defn touchCancel
+  ([element]
+   (.-touchCancel fireEvent element))
+  ([element options]
+   (.-touchCancel fireEvent element options)))
+
+(defn touchEnd
+  ([element]
+   (.-touchEnd fireEvent element))
+  ([element options]
+   (.-touchEnd fireEvent element options)))
+
+(defn touchMove
+  ([element]
+   (.-touchMove fireEvent element))
+  ([element options]
+   (.-touchMove fireEvent element options)))
+
+(defn touchStart
+  ([element]
+   (.-touchStart fireEvent element))
+  ([element options]
+   (.-touchStart fireEvent element options)))
+
+(defn scroll
+  ([element]
+   (.-scroll fireEvent element))
+  ([element options]
+   (.-scroll fireEvent element options)))
+
+(defn wheel
+  ([element]
+   (.-wheel fireEvent element))
+  ([element options]
+   (.-wheel fireEvent element options)))
+
+(defn abort
+  ([element]
+   (.-abort fireEvent element))
+  ([element options]
+   (.-abort fireEvent element options)))
+
+(defn canPlay
+  ([element]
+   (.-canPlay fireEvent element))
+  ([element options]
+   (.-canPlay fireEvent element options)))
+
+(defn canPlayThrough
+  ([element]
+   (.-canPlayThrough fireEvent element))
+  ([element options]
+   (.-canPlayThrough fireEvent element options)))
+
+(defn durationChange
+  ([element]
+   (.-durationChange fireEvent element))
+  ([element options]
+   (.-durationChange fireEvent element options)))
+
+(defn emptied
+  ([element]
+   (.-emptied fireEvent element))
+  ([element options]
+   (.-emptied fireEvent element options)))
+
+(defn encrypted
+  ([element]
+   (.-encrypted fireEvent element))
+  ([element options]
+   (.-encrypted fireEvent element options)))
+
+(defn ended
+  ([element]
+   (.-ended fireEvent element))
+  ([element options]
+   (.-ended fireEvent element options)))
+
+(defn loadedData
+  ([element]
+   (.-loadedData fireEvent element))
+  ([element options]
+   (.-loadedData fireEvent element options)))
+
+(defn loadedMetadata
+  ([element]
+   (.-loadedMetadata fireEvent element))
+  ([element options]
+   (.-loadedMetadata fireEvent element options)))
+
+(defn loadStart
+  ([element]
+   (.-loadStart fireEvent element))
+  ([element options]
+   (.-loadStart fireEvent element options)))
+
+(defn pause
+  ([element]
+   (.-pause fireEvent element))
+  ([element options]
+   (.-pause fireEvent element options)))
+
+(defn play
+  ([element]
+   (.-play fireEvent element))
+  ([element options]
+   (.-play fireEvent element options)))
+
+(defn playing
+  ([element]
+   (.-playing fireEvent element))
+  ([element options]
+   (.-playing fireEvent element options)))
+
+(defn progress
+  ([element]
+   (.-progress fireEvent element))
+  ([element options]
+   (.-progress fireEvent element options)))
+
+(defn rateChange
+  ([element]
+   (.-rateChange fireEvent element))
+  ([element options]
+   (.-rateChange fireEvent element options)))
+
+(defn seeked
+  ([element]
+   (.-seeked fireEvent element))
+  ([element options]
+   (.-seeked fireEvent element options)))
+
+(defn seeking
+  ([element]
+   (.-seeking fireEvent element))
+  ([element options]
+   (.-seeking fireEvent element options)))
+
+(defn stalled
+  ([element]
+   (.-stalled fireEvent element))
+  ([element options]
+   (.-stalled fireEvent element options)))
+
+(defn suspend
+  ([element]
+   (.-suspend fireEvent element))
+  ([element options]
+   (.-suspend fireEvent element options)))
+
+(defn timeUpdate
+  ([element]
+   (.-timeUpdate fireEvent element))
+  ([element options]
+   (.-timeUpdate fireEvent element options)))
+
+(defn volumeChange
+  ([element]
+   (.-volumeChange fireEvent element))
+  ([element options]
+   (.-volumeChange fireEvent element options)))
+
+(defn waiting
+  ([element]
+   (.-waiting fireEvent element))
+  ([element options]
+   (.-waiting fireEvent element options)))
+
+(defn load
+  ([element]
+   (.-load fireEvent element))
+  ([element options]
+   (.-load fireEvent element options)))
+
+(defn error
+  ([element]
+   (.-error fireEvent element))
+  ([element options]
+   (.-error fireEvent element options)))
+
+(defn animationStart
+  ([element]
+   (.-animationStart fireEvent element))
+  ([element options]
+   (.-animationStart fireEvent element options)))
+
+(defn animationEnd
+  ([element]
+   (.-animationEnd fireEvent element))
+  ([element options]
+   (.-animationEnd fireEvent element options)))
+
+(defn animationIteration
+  ([element]
+   (.-animationIteration fireEvent element))
+  ([element options]
+   (.-animationIteration fireEvent element options)))
+
+(defn transitionEnd
+  ([element]
+   (.-transitionEnd fireEvent element))
+  ([element options]
+   (.-transitionEnd fireEvent element options)))
+
+(defn doubleClick
+  ([element]
+   (.-doubleClick fireEvent element))
+  ([element options]
+   (.-doubleClick fireEvent element options)))
+
+(defn pointerOver
+  ([element]
+   (.-pointerOver fireEvent element))
+  ([element options]
+   (.-pointerOver fireEvent element options)))
+
+(defn pointerEnter
+  ([element]
+   (.-pointerEnter fireEvent element))
+  ([element options]
+   (.-pointerEnter fireEvent element options)))
+
+(defn pointerDown
+  ([element]
+   (.-pointerDown fireEvent element))
+  ([element options]
+   (.-pointerDown fireEvent element options)))
+
+(defn pointerMove
+  ([element]
+   (.-pointerMove fireEvent element))
+  ([element options]
+   (.-pointerMove fireEvent element options)))
+
+(defn pointerUp
+  ([element]
+   (.-pointerUp fireEvent element))
+  ([element options]
+   (.-pointerUp fireEvent element options)))
+
+(defn pointerCancel
+  ([element]
+   (.-pointerCancel fireEvent element))
+  ([element options]
+   (.-pointerCancel fireEvent element options)))
+
+(defn pointerOut
+  ([element]
+   (.-pointerOut fireEvent element))
+  ([element options]
+   (.-pointerOut fireEvent element options)))
+
+(defn pointerLeave
+  ([element]
+   (.-pointerLeave fireEvent element))
+  ([element options]
+   (.-pointerLeave fireEvent element options)))
+
+(defn gotPointerCapture
+  ([element]
+   (.-gotPointerCapture fireEvent element))
+  ([element options]
+   (.-gotPointerCapture fireEvent element options)))
+
+(defn lostPointerCapture
+  ([element]
+   (.-lostPointerCapture fireEvent element))
+  ([element options]
+   (.-lostPointerCapture fireEvent element options)))
 ;; End - Generated Code (Do not modify manually)
