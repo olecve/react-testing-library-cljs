8e6ab3aaf2ef66320657b89791cf29bd7488b684
Oleg Tsvetkov <olecve@gmail.com>
2023-07-17 12:41:28 +0300

    Fix one arity template

--- scripts/fire_event_fn.template.clj
+++ scripts/fire_event_fn.template.clj
@@ -1,5 +1,5 @@
 (defn $cljs-fn-name
   ([element]
-   (.-$js-event-type fireEvent element))
+   (.$js-event-type fireEvent element))
   ([element options]
-   (.-$js-event-type fireEvent element options)))
+   (.$js-event-type fireEvent element options)))
--- scripts/reagent.fire_event_fn.template.clj
+++ scripts/reagent.fire_event_fn.template.clj
@@ -1,6 +1,7 @@
 (defn $cljs-fn-name
   ([element]
-   (.-$js-event-type fireEvent element))
+   (.$js-event-type fireEvent element)
+   (r/flush))
   ([element options]
-   (.-$js-event-type fireEvent element options)
+   (.$js-event-type fireEvent element options)
    (r/flush)))
