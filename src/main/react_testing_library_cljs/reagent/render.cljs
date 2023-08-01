c2659308dbe79a8a93ec16c7163e2b147c9041f3
Oleg Tsvetkov <olecve@gmail.com>
2023-08-01 18:45:42 +0300

    Add render helper

---
+++ src/main/react_testing_library_cljs/reagent/render.cljs
@@ -0,0 +1,15 @@
+(ns react-testing-library-cljs.reagent.render
+  (:require ["@testing-library/react" :as testing-library]
+            [reagent.core :as r]))
+
+(def test-container-id "test-container")
+
+(defn testing-container []
+  (some-> (js/document.getElementById test-container-id) .remove)
+  (js/document.body.appendChild (doto (js/document.createElement "div")
+                                  (.setAttribute "data-testid" test-container-id)
+                                  (.setAttribute "id" test-container-id))))
+
+(defn render! [hiccup]
+  (testing-library/render (r/as-element hiccup)
+                          #js {:container (testing-container)}))
