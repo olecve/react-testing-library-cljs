c5f00187eadbbe6b224e93c131734644ac64c0bf
Oleg Tsvetkov <olecve@gmail.com>
2024-05-27 14:32:22 +0300

    Version 0.0.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.olecve/react-testing-library-cljs "0.0.15"
+(defproject org.clojars.olecve/react-testing-library-cljs "0.0.16-SNAPSHOT"
   :description "ClojureScript wrapper for react-testing-library"
   :url "https://github.com/olecve/react-testing-library-cljs"
 
