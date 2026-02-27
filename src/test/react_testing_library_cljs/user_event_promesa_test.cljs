d6f807ca133222307c766bc3d556d87b9ad76be6
Oleg Tsvetkov <oleg@iprally.com>
2026-02-27 17:56:52 +0200

    Update the deftest-async

--- src/test/react_testing_library_cljs/user_event_promesa_test.cljs
+++ src/test/react_testing_library_cljs/user_event_promesa_test.cljs
@@ -17,45 +17,34 @@
     (render-el (react/createElement "button"
                  #js {:onClick #(swap! clicks inc)}
                  "Click me"))
-    (let [user (user-event/setup)]
-      (p/do
-        (user-event/click user (screen/get-by-role "button"))
-        (is (= 1 @clicks))))))
+    (p/do
+      (user-event/click (user-event/setup) (screen/get-by-role "button"))
+      (is (= 1 @clicks)))))
 
 (deftest-async type-test
   (rtl/cleanup)
   (render-el (react/createElement "input"
                #js {:placeholder "type here"
                     :defaultValue ""}))
-  (let [user (user-event/setup)
-        input (screen/get-by-placeholder-text "type here")]
-    (p/do
-      (user-event/type user input "hello")
-      (is (= "hello" (.-value input))))))
+  (user-event/type (user-event/setup) (screen/get-by-placeholder-text "type here") "hello")
+  (is (= "hello" (.-value (screen/get-by-placeholder-text "type here")))))
 
 (deftest-async clear-test
   (rtl/cleanup)
   (render-el (react/createElement "input"
                #js {:placeholder "to clear"
                     :defaultValue "existing text"}))
-  (let [user (user-event/setup)
-        input (screen/get-by-placeholder-text "to clear")]
-    (p/do
-      (user-event/clear user input)
-      (is (= "" (.-value input))))))
+  (user-event/clear (user-event/setup) (screen/get-by-placeholder-text "to clear"))
+  (is (= "" (.-value (screen/get-by-placeholder-text "to clear")))))
 
 (deftest-async tab-test
   (rtl/cleanup)
   (render-el (react/createElement "div" nil
                (react/createElement "input" #js {:placeholder "first"})
                (react/createElement "input" #js {:placeholder "second"})))
-  (let [user (user-event/setup)
-        first-input (screen/get-by-placeholder-text "first")
-        second-input (screen/get-by-placeholder-text "second")]
-    (.focus first-input)
-    (p/do
-      (user-event/tab user)
-      (is (= second-input (.-activeElement js/document))))))
+  (.focus (screen/get-by-placeholder-text "first"))
+  (user-event/tab (user-event/setup))
+  (is (= (screen/get-by-placeholder-text "second") (.-activeElement js/document))))
 
 (deftest-async keyboard-test
   (rtl/cleanup)
@@ -65,11 +54,10 @@
                                   (.preventDefault e)
                                   (reset! submitted true))}
                  (react/createElement "input" #js {:placeholder "press enter"})))
-    (let [user (user-event/setup)]
-      (.focus (screen/get-by-placeholder-text "press enter"))
-      (p/do
-        (user-event/keyboard user "{Enter}")
-        (is (true? @submitted))))))
+    (.focus (screen/get-by-placeholder-text "press enter"))
+    (p/do
+      (user-event/keyboard (user-event/setup) "{Enter}")
+      (is (true? @submitted)))))
 
 (deftest-async select-options-test
   (rtl/cleanup)
@@ -77,8 +65,5 @@
                #js {:defaultValue ""}
                (react/createElement "option" #js {:value "a"} "Option A")
                (react/createElement "option" #js {:value "b"} "Option B")))
-  (let [user (user-event/setup)
-        select (screen/get-by-role "combobox")]
-    (p/do
-      (user-event/select-options user select "Option A")
-      (is (= "a" (.-value select))))))
+  (user-event/select-options (user-event/setup) (screen/get-by-role "combobox") "Option A")
+  (is (= "a" (.-value (screen/get-by-role "combobox")))))
