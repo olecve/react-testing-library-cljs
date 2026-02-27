4765703a544c513e6a8f3eabab12577d31ef9783
Oleg Tsvetkov <oleg@iprally.com>
2026-02-27 17:33:39 +0200

    Add deftest-async macros to wrap async test with promesa

---
+++ src/main/react_testing_library_cljs/async.clj
@@ -0,0 +1,21 @@
+(ns react-testing-library-cljs.async)
+
+(defmacro deftest-async
+  "Like `cljs.test/deftest` but for async tests that return a Promise.
+  Wraps the body in `cljs.test/async` and automatically calls `done`
+  when the returned Promise resolves.
+
+  Requires `promesa` on the classpath. Use `promesa.core/p/let` to
+  sequence async operations without nesting `.then` calls:
+
+    (deftest-async my-test
+      (p/let [_ (user-event/type user input \"hello\")]
+        (is (= \"hello\" (.-value input)))))"
+  [name & body]
+  `(cljs.test/deftest ~name
+     (cljs.test/async done#
+       (-> (do ~@body)
+           (.then done#)
+           (.catch (fn [err#]
+                     (cljs.test/is (nil? err#) (.-message err#))
+                     (done#)))))))
---
+++ src/main/react_testing_library_cljs/async.cljs
@@ -0,0 +1,2 @@
+(ns react-testing-library-cljs.async
+  (:require-macros [react-testing-library-cljs.async]))
